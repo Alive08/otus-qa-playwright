8992ef5bca6239cba02ea5e3e99900141325e5c3
Vadim Kozlov <diginet@mail.ru>
2022-08-04 01:13:11 +0700

    add attachments to allure report

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
+import time
 from pathlib import Path
+from datetime import datetime
 
+import allure
 import pytest
+from allure_commons.types import AttachmentType
 from frame.classes import Creds
 from frame.logger import _init_logger
 from pages.admin.login_page import AdminLogin
@@ -48,8 +52,8 @@ def log(request, _logger):
 
 
 @pytest.fixture(scope='session')
-def screenshots_dir(driver, rootdir, _logger):
-    workdir = Path(rootdir, "artifacts/screenshots", driver.session_id)
+def screenshots_dir(rootdir, _logger):
+    workdir = Path(rootdir, "artifacts/screenshots")
     try:
         workdir.mkdir(parents=True, exist_ok=True)
     except Exception as e:
@@ -76,3 +80,52 @@ def admin_login_page(page: Page):
     login_page.open()
     expect(login_page.page).to_have_title(login_page.login_title)
     return login_page
+
+
+# https://docs.pytest.org/en/latest/example/simple.html#post-process-test-reports-failures
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # execute all other hooks to obtain the report object
+    outcome = yield
+
+    rep = outcome.get_result()
+
+    # set a report attribute for each phase of a call
+    # which can be "setup", "call", "teardown"
+
+    setattr(item, "rep_" + rep.when, rep)
+
+
+def take_screenshot_playwright(page: Page, dir, nodeid):
+    time.sleep(1)
+    file_name = f'{nodeid}_{datetime.today().strftime("%Y-%m-%d_%H:%M")}.png'.replace("/",
+                                                                                      "_").replace("::", "__")
+
+    with open(f"{dir}/{file_name}", 'wb') as bin_file:
+        bin_file.write(page.screenshot(full_page=True))
+
+    return file_name
+
+
+def take_screenshot_allure(page: Page, nodeid):
+    # time.sleep(1)
+    allure.attach(page.screenshot(full_page=True, type='png'), name=nodeid,
+                  attachment_type=AttachmentType.PNG)
+
+
+@pytest.fixture(scope="function", autouse=True)
+def fail_check(request, page, _logger, screenshots_dir):
+
+    yield
+
+    if request.node.rep_setup.failed:
+        _logger.error("setting up a test %s failed", request.node.nodeid)
+    elif request.node.rep_setup.passed:
+        if request.node.rep_call.failed:
+            # driver = request.node.funcargs['driver']
+            screenshot = take_screenshot_playwright(
+                page, screenshots_dir, request.node.nodeid)
+            take_screenshot_allure(page, request.node.nodeid)
+            _logger.error("executing test %s failed", request.node.nodeid)
+            _logger.info("screenshot saved in %s",
+                         Path(screenshots_dir, screenshot))
--- tests/test_fail.py
+++ tests/test_fail.py
@@ -2,7 +2,7 @@ import pytest
 from playwright.sync_api import expect
 
 
-@pytest.mark.xfail
+# @pytest.mark.xfail
 def test_fail(page):
     page.goto('/')
     expect(page).not_to_have_title('Your Store')
