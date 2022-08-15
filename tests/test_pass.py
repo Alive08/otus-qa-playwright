4eed5f15b778e7fb40b501bc7999cd0c27e9a5e9
“Vadim <diginet@mail.ru>
2022-08-16 00:45:59 +0700

    unskip test_pass

--- tests/test_pass.py
+++ tests/test_pass.py
@@ -3,7 +3,7 @@ from pages.customer.main_page import MainPage
 from playwright.sync_api import Page, expect
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_pass(page: Page):
     page.goto('/')
     expect(page).to_have_title(MainPage(page).title)
