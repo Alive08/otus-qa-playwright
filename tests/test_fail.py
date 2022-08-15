74e5dc12332e91af7e6a63fa59da2119025619f2
“Vadim <diginet@mail.ru>
2022-08-15 21:23:51 +0700

    tweak test fail

--- tests/test_fail.py
+++ tests/test_fail.py
@@ -3,7 +3,7 @@ from pages.customer.main_page import MainPage
 from playwright.sync_api import Page, expect
 
 
-@pytest.mark.xfail(run=False)
+@pytest.mark.xfail
 def test_fail(page: Page):
     page.goto('/')
-    expect(page).not_to_have_title(MainPage(page).title)
+    expect(page).not_to_have_title(MainPage(page).title, timeout=2)
