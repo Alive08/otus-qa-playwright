e7dbe1389ba8c1135fb458e912dda48807ff5918
Vadim Kozlov <diginet@mail.ru>
2022-08-17 01:45:08 +0700

    tweak viewport

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,6 +20,17 @@ def options():
     return option
 
 
+@pytest.fixture(scope="session")
+def browser_context_args(browser_context_args):
+    return {
+        **browser_context_args,
+        "viewport": {
+            "width": 1920,
+            "height": 1080,
+        }
+    }
+
+
 @pytest.fixture(scope='session')
 def my_IP(request):
     return request.config.getoption("--myip")
